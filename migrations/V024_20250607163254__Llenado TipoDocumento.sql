9d1d85a7247c7e2ba0952df42ed55b61a04ad07f
Elías Ramírez Hernández <eliasramirezhernandez1@gmail.com>
2025-06-07 16:33:36 -0600

    llenado toronjita

---
+++ migrations/V024_20250607163254__Llenado TipoDocumento.sql
@@ -0,0 +1,12 @@
+INSERT INTO dbo.pv_tipoDocumento (nombre)
+VALUES
+    ('Cédula de Identidad'),
+    ('Pasaporte'),
+    ('Licencia de Conducir'),
+    ('Documento Legal'),
+    ('Acta de Nacimiento'),
+    ('Certificado de Votación'),
+    ('Factura'),
+    ('Contrato'),
+    ('Recibo'),
+    ('Comprobante');

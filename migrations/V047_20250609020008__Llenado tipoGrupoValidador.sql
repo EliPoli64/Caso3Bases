c16c926942af2c212cc043aae5cadcbf52ca3c40
pinkcrate <b.diaz.1@estudiantec.cr>
2025-06-09 02:00:40 -0600

    tipoGrupoValidador

---
+++ migrations/V047_20250609020008__Llenado tipoGrupoValidador.sql
@@ -0,0 +1,12 @@
+INSERT INTO pv_tipoGrupoValidador (nombreTipo) VALUES
+('Validadores Ciudadanos'),
+('Aceleradoras'),
+('Incubadoras'),
+('Grupos Inversores'),
+('Comité Técnico MICITT'),
+('Evaluadores Hacienda'),
+('Consejo Social'),
+('Comité Ambiental'),
+('Validadores Documentación'),
+('Fiscalizadores Ejecución'),
+('Comité Ético');

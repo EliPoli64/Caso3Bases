5f3308c7f7c26765025448da527e06bff70206b5
LaPlanta1605 <167035555+Jcastro1605@users.noreply.github.com>
2025-06-09 13:13:37 -0600

    Estado Proyecto

---
+++ caso3BD/migrations/V006_20250609131312__Estado Proyecto.sql
@@ -0,0 +1,8 @@
+INSERT INTO pv_estadoProyecto
+VALUES ('Propuesta'), 
+('Aprobado'),('En Pausa'), 
+('Cancelado'),('Rechazado'), 
+('Revisión'),('Pruebas'), 
+('Implementación'),('Postergado'), 
+('Esperando Aprobación'), 
+('Archivado');

479954fd7dbb0a057ed77b3098084b06eaf7e8d9
CGarcia1411 <carlos.andres.garcia.molina.2005@gmail.com>
2025-06-08 21:47:20 -0600

    Arreglo de Segmentos
    Co-Authored-By: Elías Ramírez Hernández <168288076+EliPoli64@users.noreply.github.com>

--- migrations/V018_20250607124044__Llenado Segmento, SeveridadLogs, TipoDetalle, TipoPregunta, EstadoComentarios.sql
+++ migrations/V018_20250607124044__Llenado Segmento, SeveridadLogs, TipoDetalle, TipoPregunta, EstadoComentarios.sql
@@ -1,12 +1,17 @@
-INSERT INTO dbo.pv_segmento (nombre) VALUES
-('General'),
-('Jóvenes (18-30)'),
-('Adultos (31-60)'),
-('Adultos Mayores (61+)'),
-('Residentes Urbanos'),
-('Residentes Rurales'),
-('Padres de Familia'),
-('Estudiantes Universitarios');
+INSERT INTO dbo.pv_segmento (nombre, descripcion, enabled, deleted, fechaCreacion, usuarioID) VALUES
+('General', 'Segmento general', 1, 0, GETDATE(), 456),
+('Jóvenes (18-30)', 'Segmento para las personas entre 18 y 30 años', 1, 0, GETDATE(), 789),
+('Adultos (31-60)', 'Segmento para las personas entre 30 y 60 años', 1, 0, GETDATE(), 32),
+('Adultos Mayores (61+)' , 'Segmento para las personas mayores a 61 años', 1, 0, GETDATE(), 156),
+('Heredianas', 'Segmento para las personas nacidas en Heredia', 1, 0, GETDATE(), 950),
+('Josefinas', 'Segmento para las personas nacidas en San José', 1, 0, GETDATE(), 950),
+('Estudiantes Tec', 'Segmento para los estudiantes del tecnológico de Costa Rica', 1, 0, GETDATE(), 789);
+INSERT INTO dbo.pv_tipoPregunta (tipo) VALUES
+('Opción Múltiple'),
+('Respuesta Abierta'),
+('Escala Likert'),
+('Sí/No'),
+('Numérica');
 INSERT INTO dbo.pv_severidadLogs (nombre) VALUES
 ('Debug'),
 ('Info'),
@@ -25,18 +30,12 @@ INSERT INTO dbo.pv_tipoDetalle (Nombre) VALUES
 ('Nivel de Satisfacción'),
 ('Frecuencia de Uso'),
 ('Motivo de Contacto'),
-('Tipo de Servicio');
+('Tipo de Servicio'),
 ('Afiliación'),
 ('Género');
-INSERT INTO dbo.pv_tipoPregunta (tipo) VALUES
-('Opción Múltiple'),
-('Respuesta Abierta'),
-('Escala Likert'),
-('Sí/No'),
-('Numérica');
-INSERT INTO dbo.pv_estadoComentarios (estadoComentid, nombre) VALUES
-(1, 'Pendiente de Revisión'),
-(2, 'Aprobado'),
-(3, 'Rechazado'),
-(4, 'Archivado'),
-(5, 'Respondido');
+INSERT INTO dbo.pv_estadoComentarios ( nombre) VALUES
+('Pendiente de Revisión'),
+('Aprobado'),
+('Rechazado'),
+('Archivado'),
+('Respondido');

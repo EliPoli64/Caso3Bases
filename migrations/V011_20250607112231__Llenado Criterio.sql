c6438cb65d7b03bfb7dd70c38dcb1aa071a2a134
CGarcia1411 <carlos.andres.garcia.molina.2005@gmail.com>
2025-06-08 22:13:16 -0600

    Arreglo segmentos, criterio y detalle Segmento

--- migrations/V011_20250607112231__Llenado Criterio.sql
+++ migrations/V011_20250607112231__Llenado Criterio.sql
@@ -1,9 +1,23 @@
-INSERT INTO dbo.pv_criterio (nombre, comparador, valor) VALUES
-('Edad Mínima', '>=', 18),
-('Edad Máxima', '<=', 65),
-('Ingreso Anual Mínimo', '>=', 25000),
-('Educación Mínima', '>=', 3), -- 3 es universitario o mayor
-('Género Masculino', '=', 1),
-('Género Femenino', '=', 0),
-('Nacionalidad Costarricense', '=', 1),
-('Participaciones en Eventos', '>=', 2);
+INSERT INTO pv_detalleSegmento(segmentoID, tipoDetalleID, tabla, campo)
+VALUES
+(2,2, 'pv_usuarios', 'fechaNacimiento - Realizar conversión'),
+(3,2, 'pv_usuarios', 'fechaNacimiento - Realizar conversión'),
+(4,2, 'pv_usuarios', 'fechaNacimiento - Realizar conversión'),
+(5,3, 'pv_provincias', 'provinciaid'),
+(6,3, 'pv_provincias', 'provinciaid'),
+(7,5, 'pv_nivelEducacion', 'nivelID'),
+(8,12, 'pv_afiliaciones', 'afiliacionID')
+;
+
+INSERT INTO dbo.pv_criterio (nombre, comparador, valor, detalleID) VALUES
+('Mayor a 18', '>=', 18, 1),
+('Menor a 30', '<=', 30, 1),
+('Mayor a 30', '>', 30, 2),
+('Menor a 60', '<=', 60, 2),
+('Mayor a 60', '>', 60, 3),
+('Heredianas', '=', 4, 4),
+('Josefinas', '=', 1, 5),
+('Universitarios', '>', 2, 6),
+('Estudiantes TEC', '=', 2,7)
+;
+
--- migrations/V018_20250607124044__Llenado Segmento, SeveridadLogs, TipoDetalle, TipoPregunta, EstadoComentarios.sql
+++ migrations/V018_20250607124044__Llenado Segmento, SeveridadLogs, TipoDetalle, TipoPregunta, EstadoComentarios.sql
@@ -1,10 +1,11 @@
 INSERT INTO dbo.pv_segmento (nombre, descripcion, enabled, deleted, fechaCreacion, usuarioID) VALUES
-('General', 'Segmento general', 1, 0, GETDATE(), 456),
+('General', 'Segmento general', 1, 0, GETDATE(), 456), 
 ('Jóvenes (18-30)', 'Segmento para las personas entre 18 y 30 años', 1, 0, GETDATE(), 789),
 ('Adultos (31-60)', 'Segmento para las personas entre 30 y 60 años', 1, 0, GETDATE(), 32),
 ('Adultos Mayores (61+)' , 'Segmento para las personas mayores a 61 años', 1, 0, GETDATE(), 156),
 ('Heredianas', 'Segmento para las personas nacidas en Heredia', 1, 0, GETDATE(), 950),
 ('Josefinas', 'Segmento para las personas nacidas en San José', 1, 0, GETDATE(), 950),
+('Educación universitaria', 'Segmento para las personas nacidas con educación universitaria completa', 1, 0, GETDATE(), 14),
 ('Estudiantes Tec', 'Segmento para los estudiantes del tecnológico de Costa Rica', 1, 0, GETDATE(), 789);
 INSERT INTO dbo.pv_tipoPregunta (tipo) VALUES
 ('Opción Múltiple'),

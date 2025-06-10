9e251fff6a7a9597fc4a2fd71940c07a0813bcc3
CGarcia1411 <carlos.andres.garcia.molina.2005@gmail.com>
2025-06-09 22:35:11 -0600

    Llenado peso respuesta.

---
+++ migrations/V057_20250609223435__Llenado Peso respuesta.sql
@@ -0,0 +1,10 @@
+INSERT INTO dbo.pv_pesoRespuesta (nombre, multiplicador) VALUES
+('Doble', 2.00),
+('Normal', 1.00),
+('No Válido', 0.00),
+('Triple', 3.00),
+('Uno y medio', 1.50),
+('Un cuarto', 0.25),
+('Un medio', 0.50),
+('Tres cuartos', 0.75),
+('Por 10', 10.00);
